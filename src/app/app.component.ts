2073c5aa5e8b2e7fdb656abbed765290a0e35309
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-17 16:59:03 +0530

    input-output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'SampleAPP';
   developers = ["Narayan","Hari","Joseph","Alex","Naveen"]
+  theme ="theme1"
+
+  changeTheme(){
+    this.theme= this.theme=="theme1"? "theme2":"theme1";
+  }
+
 }
