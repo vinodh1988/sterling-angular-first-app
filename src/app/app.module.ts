185ecf7f20dd61bcb45575d520ef748d6a2bb5cd
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-17 16:20:48 +0530

    new-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './components/box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent //components, directives and pipes need to be specified
+    AppComponent,
+    BoxComponent //components, directives and pipes need to be specified
   ],
   imports: [
     BrowserModule,
